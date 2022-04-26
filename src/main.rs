79b9337443c7f63e6a4a75f227f7755777c370f1
scfcode <scf.code@gmail.com>
2022-04-25 18:16:27 -0700

    Minor fixups
    Very minor fixes post a rustfmt

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,15 @@
 use mountpoints::mountpaths;
 
 fn main() {
-    println!("Diplaying Mountpoints");
+    println!("Mount Path Info");
 
     let d: Disks = Disks::new();
     d.print_mount_list();
 }
 
-
 #[derive(Debug)]
 struct Disks {
-    disk_vector: Vec<String>,  // A vector of Strings
+    disk_vector: Vec<String>, // A Vec vector of String types
 }
 
 impl Disks {
@@ -24,18 +23,17 @@ impl Disks {
     }
 
     fn build_mount_list() -> Vec<String> {
-
         let mut v: Vec<String> = vec![];
 
-        for mp in mountpaths().unwrap() {   // Build the Vec of mountpath strings
+        for mp in mountpaths().unwrap() {
+            // Build the Vec of mount path strings
             v.push(mp);
         }
 
-        v    // Return our Vec
+        v // Return our Vec
     }
 
     fn print_mount_list(&self) {
-
         let mut index: usize = 0;
 
         while index < self.disk_vector.len() {
